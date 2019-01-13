25ab8ff8f484975700a414558017c9b5214989ae
wangchao <magician.of.technique@gmail.com>
2019-01-13 09:05:18 +0800

    [*] App single instance

--- app/src/main/java/com/secingot/quick/app/App.kt
+++ app/src/main/java/com/secingot/quick/app/App.kt
@@ -10,7 +10,16 @@ import android.app.Application
  */
 class App : Application() {
 
+    companion object {
+        private lateinit var app: App
+
+        fun get(): App {
+            return app
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
+        app = this
     }
 }
